61c87eb6cf322fa4d6f042157f87344c485697dd
wolanski <kacpertomasz00@gmail.com>
2022-10-31 10:56:24 +0100

    topmenu ok

--- portfolio/scripts/top_menu.js
+++ portfolio/scripts/top_menu.js
@@ -9,16 +9,21 @@ open_site_menu_btn.addEventListener("click", () =>
 {
     const containerH = site_menu.getBoundingClientRect().height
     const btnsH = site_menu_btns.getBoundingClientRect().height
-
-    if(containerH === 0 )
+    var btn_opacity = window.getComputedStyle(open_site_menu_btn).getPropertyValue("opacity")
+    
+    if(btn_opacity != 0)
     {
-        site_menu.style.height = `${btnsH}px`
-        site_menu.classList.toggle('show_site_menu')
+        if(containerH === 0)
+        {
+            site_menu.style.height = `${btnsH}px`
+            site_menu.classList.toggle('show_site_menu')
+        }
+        else 
+        {
+            site_menu.style.height = 0
+        }
     }
-    else
-    {
-        site_menu.style.height = 0
-    }
+    
 })
 
 window.addEventListener("resize", function()
@@ -28,7 +33,7 @@ window.addEventListener("resize", function()
         //$(top_menu_btns).css('visibility', 'hidden')
         $(top_menu_btns).css('opacity', '0')
 
-        //$(open_site_menu_btn).css('visibility', 'visible')
+        //$(open_site_menu_btn).css('display', 'block')
         $(open_site_menu_btn).css('opacity', '1')
     }
     else  
@@ -36,7 +41,8 @@ window.addEventListener("resize", function()
         //$(top_menu_btns).css('visibility','visible')
         $(top_menu_btns).css('opacity', '1')
 
-        //$(open_site_menu_btn).css('visibility', 'hidden') 
+        //$(open_site_menu_btn).css('display', 'none') 
         $(open_site_menu_btn).css('opacity', '0')
+        site_menu.style.height = 0
     }
 })
